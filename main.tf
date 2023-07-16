3708e276189fab426b5f37b17e19c55061d72833
Tristan Self <34382899+tristanhself@users.noreply.github.com>
2023-07-16 20:06:11 +0100

    create security group with fix

--- main.tf
+++ main.tf
@@ -43,7 +43,6 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port           = 80
   protocol          = tcp
   cidr_blocks       = ["0.0.0.0/0"]
-
   security_group_id = aws_security_group.blog.id
 }
 
@@ -53,7 +52,6 @@ resource "aws_security_group_rule" "blog_https_in" {
   to_port           = 443
   protocol          = tcp
   cidr_blocks       = ["0.0.0.0/0"]
-
   security_group_id = aws_security_group.blog.id
 }
 
@@ -63,6 +61,5 @@ resource "aws_security_group_rule" "blog_everything_out" {
   to_port           = 0
   protocol          = -1
   cidr_blocks       = ["0.0.0.0/0"]
-
   security_group_id = aws_security_group.blog.id
 }
