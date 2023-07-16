4d0147da2b2e7d6891ea3a3db368ed0086eb6be5
Tristan Self <34382899+tristanhself@users.noreply.github.com>
2023-07-16 20:09:47 +0100

    create security group with fix 3

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
-  protocol          = tcp
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "aws_security_group.blog.id"
 }
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   type              = "ingress"
   from_port         = 443
   to_port           = 443
-  protocol          = tcp
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "aws_security_group.blog.id"
 }
@@ -59,7 +59,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
-  protocol          = -1
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "aws_security_group.blog.id"
 }
