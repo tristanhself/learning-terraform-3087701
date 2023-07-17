8834c1f638ef673c3347b84deb2a117267c0f09f
Tristan Self <34382899+tristanhself@users.noreply.github.com>
2023-07-17 20:17:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,16 +45,15 @@ resource "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-/*
 resource "aws_security_group_rule" "blog_https_in" {
   type              = "ingress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "aws_security_group.blog.id"
+  security_group_id = aws_security_group.blog.id
 }
-
+/*
 resource "aws_security_group_rule" "blog_everything_out" {
   type              = "egress"
   from_port         = 0
