36b1d6970f8dddd5f542189ede113c8d72ce8996
Tristan Self <34382899+tristanhself@users.noreply.github.com>
2023-07-18 21:53:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
+/*
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow HTTP and HTTPS in, allow everything out"
@@ -75,3 +76,4 @@ resource "aws_security_group_rule" "blog_everything_out" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
+*/
